650226bbf05e25d211b8570d7c9c98df35a00723
LerkKate <3ai42@mail.ru>
2023-09-12 22:07:52 +0300

    перенесены шаги из теста в метод инициализации фикстуры

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/UntitledTestCase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/UntitledTestCase.java
@@ -20,10 +20,6 @@ public class UntitledTestCase {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) wd;
-  }
-
-  @Test
-  public void testUntitledTestCase() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("user")).click();
@@ -33,6 +29,10 @@ public class UntitledTestCase {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
